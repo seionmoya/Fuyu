5f419f8919ea100f275d98dd6759eac2b44970aa
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2024-10-16 00:42:04 -0700

    Removed comments

--- Fuyu.Backend.EFT/DTO/Responses/FriendListResponse.cs
+++ Fuyu.Backend.EFT/DTO/Responses/FriendListResponse.cs
@@ -7,15 +7,12 @@ namespace Fuyu.Backend.EFT.DTO.Responses
     [DataContract]
     public class FriendListResponse
     {
-        // TODO: proper type
         [DataMember]
         public ChatRoomMember[] Friends;
 
-        // TODO: proper type
         [DataMember]
         public MongoId[] Ignore;
 
-        // TODO: proper type
         [DataMember]
         public MongoId[] InIgnoreList;
     }
