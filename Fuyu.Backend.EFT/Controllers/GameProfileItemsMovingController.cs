53e7f217131a5833db776668248d3c56d994c00a
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2024-10-22 18:51:05 -0700

    Added the player ids to the dictionary by default, possibly make this a method later instead to be added only when needed

--- Fuyu.Backend.EFT/Controllers/GameProfileItemsMovingController.cs
+++ Fuyu.Backend.EFT/Controllers/GameProfileItemsMovingController.cs
@@ -9,52 +9,57 @@ using System.Threading.Tasks;
 
 namespace Fuyu.Backend.EFT.Controllers
 {
-    public class GameProfileItemsMovingController : HttpController<JObject>
-    {
-        private ItemEventRouter _router = new ItemEventRouter();
+	public class GameProfileItemsMovingController : HttpController<JObject>
+	{
+		private ItemEventRouter _router = new ItemEventRouter();
 
-        public GameProfileItemsMovingController() : base("/client/game/profile/items/moving")
-        {
-            _router.AddController<CustomizationBuyEventController>();
-            _router.AddController<EatItemEventController>();
-            _router.AddController<InsureEventController>();
-            _router.AddController<InterGameTransferEventController>();
-            _router.AddController<MoveItemEventController>();
-            _router.AddController<ReadEncyclopediaEventController>();
-            _router.AddController<SellAllFromSavageEventController>();
-            _router.AddController<TraderRepairEventController>();
-            _router.AddController<TradingConfirmEventController>();
-            _router.AddController<ApplyInventoryChangesItemEventController>();
-        }
-
-        public override async Task RunAsync(HttpContext context, JObject request)
+		public GameProfileItemsMovingController() : base("/client/game/profile/items/moving")
 		{
-            if (!request.ContainsKey("data"))
-            {
-                return;
-            }
+			_router.AddController<CustomizationBuyEventController>();
+			_router.AddController<EatItemEventController>();
+			_router.AddController<InsureEventController>();
+			_router.AddController<InterGameTransferEventController>();
+			_router.AddController<MoveItemEventController>();
+			_router.AddController<ReadEncyclopediaEventController>();
+			_router.AddController<SellAllFromSavageEventController>();
+			_router.AddController<TraderRepairEventController>();
+			_router.AddController<TradingConfirmEventController>();
+			_router.AddController<ApplyInventoryChangesItemEventController>();
+		}
 
-			var sessionId = context.GetSessionId();
-            var requestData = request.Value<JArray>("data");
-            var response = new ItemEventResponse
-            {
-                ProfileChanges = [],
-                InventoryWarnings = []
-            };
-
-            int requestIndex = 0;
-            foreach (var itemRequest in requestData)
-            {
-                var action = itemRequest.Value<string>("Action");
-                var itemEventContext = new ItemEventContext(sessionId, action, requestIndex, itemRequest, response);
-                await _router.RouteAsync(itemEventContext);
-                requestIndex++;
+		public override async Task RunAsync(HttpContext context, JObject request)
+		{
+			if (!request.ContainsKey("data"))
+			{
+				return;
 			}
 
-            await context.SendJsonAsync(Json.Stringify(new ResponseBody<ItemEventResponse>
-            {
-                data = response
-            }));
-        }
-    }
+			var sessionId = context.GetSessionId();
+			var account = EftOrm.GetAccount(sessionId);
+			var profile = EftOrm.GetProfile(account.PveId);
+			var requestData = request.Value<JArray>("data");
+			var response = new ItemEventResponse
+			{
+				ProfileChanges = {
+					{ profile.Pmc._id, new ProfileChange() },
+					{ profile.Savage._id, new ProfileChange() }
+				},
+				InventoryWarnings = []
+			};
+
+			int requestIndex = 0;
+			foreach (var itemRequest in requestData)
+			{
+				var action = itemRequest.Value<string>("Action");
+				var itemEventContext = new ItemEventContext(sessionId, action, requestIndex, itemRequest, response);
+				await _router.RouteAsync(itemEventContext);
+				requestIndex++;
+			}
+
+			await context.SendJsonAsync(Json.Stringify(new ResponseBody<ItemEventResponse>
+			{
+				data = response
+			}));
+		}
+	}
 }
