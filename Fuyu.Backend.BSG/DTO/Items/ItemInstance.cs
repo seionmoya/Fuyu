e255de9e65ea55d2cb4bc7caf06501f68d464178
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2024-10-27 18:54:40 -0700

    Added ItemInstance.GetUpd<T> (relies on reflection)

--- Fuyu.Backend.BSG/DTO/Items/ItemInstance.cs
+++ Fuyu.Backend.BSG/DTO/Items/ItemInstance.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Runtime.Serialization;
 using Fuyu.Common.Collections;
 using Fuyu.Common.Hashing;
@@ -28,5 +30,27 @@ namespace Fuyu.Backend.EFT.DTO.Items
         // emits when 'null'
         [DataMember(EmitDefaultValue = false)]
         public ItemUpdatable upd;
+
+        public T GetUpd<T>()
+        {
+            // NOTE: ??= means assign thisUpd to upd but if upd is null create a new instance
+            // -- nexus4880, 2024-10-27
+            var thisUpd = upd ??= new ItemUpdatable();
+
+			// NOTE: Intentionally letting this throw here. The idea is that GetUpd should
+			// create T if it doesn't exist meaning most usage would be GetUpd<Upd>().Value
+            // which means a null check after calling this would be undesirable
+            // -- nexus4880, 2024-10-27
+			var field = thisUpd.GetType().GetFields().First(f => f.FieldType == typeof(T));
+            var updValue = field.GetValue(thisUpd);
+
+            if (updValue == null)
+            {
+                updValue = Activator.CreateInstance(typeof(T));
+                field.SetValue(thisUpd, updValue);
+            }
+
+            return (T)updValue;
+        }
     }
 }
