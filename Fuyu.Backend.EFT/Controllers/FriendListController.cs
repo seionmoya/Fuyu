d477484d457083a1d5c862f1352cac1a8d6432a7
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2024-10-31 10:45:22 -0700

    Remove hardcoded friend list test

--- Fuyu.Backend.EFT/Controllers/FriendListController.cs
+++ Fuyu.Backend.EFT/Controllers/FriendListController.cs
@@ -3,10 +3,6 @@ using Fuyu.Common.Networking;
 using Fuyu.Common.Serialization;
 using Fuyu.Backend.BSG.DTO.Responses;
 using Fuyu.Backend.EFT.DTO.Responses;
-using Fuyu.Backend.BSG.DTO.Friends;
-using Fuyu.Common.Hashing;
-using Fuyu.Backend.BSG.DTO.Profiles.Info;
-using System;
 
 namespace Fuyu.Backend.EFT.Controllers
 {
@@ -22,24 +18,7 @@ namespace Fuyu.Backend.EFT.Controllers
             {
                 data = new FriendListResponse()
                 {
-                    Friends = [
-                        new ChatRoomMember
-                        {
-                            Id = new MongoId(true),
-                            AccountId = int.MaxValue,
-                            Info = new ChatRoomMemberInfo
-                            {
-                                MemberCategory = EMemberCategory.Default,
-                                SelectedMemberCategory = EMemberCategory.Default,
-                                Banned = false,
-                                Ignored = false,
-                                Level = 31,
-                                Nickname = "b1gsteppa",
-                                Side = EChatMemberSide.Usec,
-                                GlobalMuteTime = DateTime.UtcNow.AddSeconds(10d)
-                            }
-                        }
-                        ],
+                    Friends = [],
                     Ignore = [],
                     InIgnoreList = []
                 }
