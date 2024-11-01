adaeae8f9952428512cd7d572be1e6878c4d623c
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2024-10-31 20:39:09 -0700

    Change BundleAddress to ResourceKey

---
+++ Fuyu.Backend.EFT/DTO/Common/ResourceKey.cs
@@ -0,0 +1,14 @@
+using System.Runtime.Serialization;
+
+namespace Fuyu.Backend.EFT.DTO.Common
+{
+    [DataContract]
+    public class ResourceKey
+    {
+        [DataMember(Name = "path")]
+        public string Path { get; set; }
+
+        [DataMember(Name = "rcid")]
+        public string RCID { get; set; }
+	}
+}
