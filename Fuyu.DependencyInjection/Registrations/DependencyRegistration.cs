9812d794247f35b5ce6f3dfa4273707a3454dd63
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2024-11-26 14:09:46 -0700

    Add DI project

---
+++ Fuyu.DependencyInjection/Registrations/DependencyRegistration.cs
@@ -0,0 +1,9 @@
+﻿namespace Fuyu.DependencyInjection.Registrations
+{
+	internal abstract class DependencyRegistration
+    {
+        public string Id { get; set; }
+
+        public abstract object GetValue();
+    }
+}
---
+++ Fuyu.DependencyInjection/Registrations/SingletonRegistration.cs
@@ -0,0 +1,18 @@
+﻿namespace Fuyu.DependencyInjection.Registrations
+{
+	internal class SingletonRegistration<T> : DependencyRegistration
+    {
+        public T Instance { get; }
+
+        internal SingletonRegistration(string id, T instance)
+        {
+            Id = id;
+            Instance = instance;
+        }
+
+		public override object GetValue()
+        {
+            return Instance;
+        }
+    }
+}
