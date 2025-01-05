b1d7c949271ff9ad2a223b0f2ee35c46fc606835
静穏靄 <170472707+seionmoya@users.noreply.github.com>
2025-01-05 01:37:08 +0100

    Simplify ConsistencyGeneralPatch

--- Fuyu.Plugin.EFT/Patches/ConsistencyGeneralPatch.cs
+++ Fuyu.Plugin.EFT/Patches/ConsistencyGeneralPatch.cs
@@ -4,27 +4,19 @@ using System.Reflection;
 using System.Threading.Tasks;
 using EFT;
 using Fuyu.Plugin.Common.Reflection;
-using Fuyu.Plugin.EFT.Reflection;
 
 namespace Fuyu.Plugin.EFT.Patches
 {
     public class ConsistencyGeneralPatch : APatch
     {
-        private static readonly MethodInfo _mi;
-
-        static ConsistencyGeneralPatch()
-        {
-            var flags = PatchHelper.PrivateFlags;
-            _mi = typeof(TarkovApplication).BaseType.GetMethod("RunFilesChecking", flags);
-        }
-
         public ConsistencyGeneralPatch() : base("com.fuyu.plugin.eft.consistencygeneral", EPatchType.Prefix)
         {
         }
 
         protected override MethodBase GetOriginalMethod()
         {
-            return _mi;
+            var flags = BindingFlags.Instance | BindingFlags.NonPublic;
+            return typeof(TarkovApplication).BaseType.GetMethod("RunFilesChecking", flags);
         }
 
         protected static bool Patch(ref Task __result)
