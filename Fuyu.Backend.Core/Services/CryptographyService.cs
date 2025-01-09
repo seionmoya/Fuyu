82d38e459ad867209a079636a9ea96d0c3d2d4f1
SlejmUr <slejmur@protonmail.com>
2025-01-09 17:34:59 +0100

    update all csproj to use LangLevel 13. (net9.0) Add AesHelper from CryptographyService Function from CryptographyService to AesHelper.

--- Fuyu.Backend.Core/Services/CryptographyService.cs
+++ Fuyu.Backend.Core/Services/CryptographyService.cs
@@ -1,44 +1,19 @@
-﻿using System.IO;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
+﻿using System.Text;
+using Fuyu.Common.Hashing;
 
 namespace Fuyu.Backend.Core.Services;
 
-// Comment: Should we move this to elsewhere?
 public static class CryptographyService
 {
     public static byte[] Key => Encoding.UTF8.GetBytes("Qo*np7*yPHqWX8ZB3ZO@m1k4");
 
     public static byte[] EncryptAes(byte[] data)
     {
-        Aes aes = Aes.Create();
-        aes.GenerateIV();
-        aes.Padding = PaddingMode.Zeros;
-        aes.Key = Key;
-        var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-        // TODO: Make this better. Maybe just aes.EncryptEcb ?
-        var memory = new MemoryStream();
-        CryptoStream cryptoStream = new CryptoStream(memory, encryptor, CryptoStreamMode.Write);
-        cryptoStream.Write(data, 0, data.Length);
-        cryptoStream.Close();
-        // EFT concat the bytes
-        return aes.IV.Concat(memory.ToArray()).ToArray();
+        return AesHelper.EncryptAes(data, Key);
     }
 
     public static byte[] DecryptAes(byte[] data)
     {
-        Aes aes = Aes.Create();
-        aes.GenerateIV();
-        aes.Padding = PaddingMode.Zeros;
-        aes.IV = data[..16];
-        aes.Key = Key;
-        var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-        // TODO: Make this better. Maybe just aes.DecryptEcb ?
-        var memory = new MemoryStream();
-        CryptoStream cryptoStream = new CryptoStream(memory, decryptor, CryptoStreamMode.Write);
-        cryptoStream.Write(data[..16], 0, data.Length - 16);
-        cryptoStream.Close();
-        return [];
+        return AesHelper.DecryptAes(data, Key);
     }
 }
