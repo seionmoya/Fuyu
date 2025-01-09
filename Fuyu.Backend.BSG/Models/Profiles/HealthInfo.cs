9bf10bdddfb7728890cf8a02bf2f4370e6f67359
Lacyway <20912169+Lacyway@users.noreply.github.com>
2025-01-09 06:51:23 +0100

    Init

--- Fuyu.Backend.BSG/Models/Profiles/HealthInfo.cs
+++ Fuyu.Backend.BSG/Models/Profiles/HealthInfo.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Runtime.Serialization;
+using Fuyu.Backend.BSG.ItemTemplates;
 using Fuyu.Backend.BSG.Models.Common;
 using Fuyu.Backend.BSG.Models.Profiles.Health;
 
@@ -21,6 +23,22 @@ namespace Fuyu.Backend.BSG.Models.Profiles
 
         [DataMember]
         public int? UpdateTime { get; set; }
+
+        public BodyPart GetBodyPart(EBodyPart bodyPart)
+        {
+            return bodyPart switch
+            {
+                EBodyPart.Head => BodyParts.Head,
+                EBodyPart.Chest => BodyParts.Chest,
+                EBodyPart.Stomach => BodyParts.Stomach,
+                EBodyPart.LeftArm => BodyParts.LeftArm,
+                EBodyPart.RightArm => BodyParts.RightArm,
+                EBodyPart.LeftLeg => BodyParts.LeftLeg,
+                EBodyPart.RightLeg => BodyParts.RightLeg,
+                EBodyPart.Common => throw new NotImplementedException("Cannot use Common?"),
+                _ => null,
+            };
+        }
 
         // SKIPPED: Immortal
         // Reason: only used on BSG's internal server
---
+++ Fuyu.Backend.EFT/Controllers/ItemEvents/HealItemEventController.cs
@@ -0,0 +1,48 @@
+﻿using Fuyu.Backend.BSG.Models.ItemEvents;
+using Fuyu.Backend.BSG.Models.Items;
+using Fuyu.Backend.BSG.Networking;
+using Fuyu.Common.IO;
+using System.Threading.Tasks;
+
+namespace Fuyu.Backend.EFT.Controllers.ItemEvents
+{
+    public class HealItemEventController : AbstractItemEventController<HealItemEvent>
+    {
+        private readonly EftOrm _eftOrm;
+
+        public HealItemEventController() : base("Heal")
+        {
+            _eftOrm = EftOrm.Instance;
+        }
+
+        // This method only finds the item, as well as the index. Actually consuming/deleting the item needs to be done.
+        public override Task RunAsync(ItemEventContext context, HealItemEvent request)
+        {
+            var profile = _eftOrm.GetActiveProfile(context.SessionId);
+            var item = profile.Pmc.Inventory.FindItem(request.Item);
+
+            if (item == null)
+            {
+                Terminal.WriteLine($"Failed to find item {request.Item}");
+                return Task.CompletedTask;
+            }
+
+            var medKit = item.GetOrCreateUpdatable<ItemMedKitComponent>();
+            if (medKit == null)
+            {
+                Terminal.WriteLine("Could not find medKit on item: " + request.Item);
+                return Task.CompletedTask;
+            }
+
+            medKit.HpResource -= request.Count;
+            if (medKit.HpResource <= 0)
+            {
+                profile.Pmc.Inventory.RemoveItem(item);
+            }
+
+            var bodyPart = profile.Pmc.Health.GetBodyPart(request.BodyPart);
+
+            return Task.CompletedTask;
+        }
+    }
+}
