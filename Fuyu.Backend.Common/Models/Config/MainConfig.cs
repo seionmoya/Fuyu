8f67043dc034afc3bf40dfff4ce15e3606dc6097
SlejmUr <slejmur@protonmail.com>
2025-01-09 19:48:07 +0100

    add simple configService

---
+++ Fuyu.Backend.Common/Models/Config/MainConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.Serialization;
+
+namespace Fuyu.Backend.Common.Models.Config;
+
+[DataContract]
+public class MainConfig
+{
+    [DataMember]
+    public WebConfig WebConfig { get; set; } = new();
+}
---
+++ Fuyu.Backend.Common/Models/Config/WebConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace Fuyu.Backend.Common.Models.Config;
+
+[DataContract]
+public class WebConfig
+{
+    [DataMember]
+    public string FuyuServerAddress { get; set; } = "http://127.0.0.1:8000";
+
+    [DataMember]
+    public string EftServerAddress { get; set; } = "http://127.0.0.1:8010";
+
+    [DataMember]
+    public string ArenaServerAddress { get; set; } = "http://127.0.0.1:8020";
+
+    // Enable us to make the server HTTPS. We need to make a certificate see below
+    [DataMember]
+    public bool UseSSL { get; set; } = false;
+
+    [DataMember]
+    public string CertificatePath { get; set; } = string.Empty;
+
+    [DataMember]
+    public string CertificatePassword { get; set; } = string.Empty;
+}
