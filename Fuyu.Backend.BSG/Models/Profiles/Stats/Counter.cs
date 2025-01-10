db2e73531bdf291de6990d3f99abbfd670049a2a
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2025-01-10 13:21:03 -0700

    Remove comment

--- Fuyu.Backend.BSG/Models/Profiles/Stats/Counter.cs
+++ Fuyu.Backend.BSG/Models/Profiles/Stats/Counter.cs
@@ -6,7 +6,6 @@ namespace Fuyu.Backend.BSG.Models.Profiles.Stats;
 [DataContract]
 public class Counter
 {
-    // TODO: proper type
     [DataMember]
     public List<KeyValuePair<List<string>, long>> Items { get; set; }
 }
