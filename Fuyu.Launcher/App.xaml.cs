f381b8e11399aecbeca9ed7f473ff1706f288531
静穏靄 <170472707+seionmoya@users.noreply.github.com>
2025-01-10 17:55:26 +0100

    Add app startup method

--- Fuyu.Launcher/App.xaml.cs
+++ Fuyu.Launcher/App.xaml.cs
@@ -4,4 +4,10 @@ namespace Fuyu.Launcher;
 
 public partial class App : Application
 {
+    void App_Startup(object sender, StartupEventArgs e)
+    {
+        // Create main application window
+        var mainWindow = new MainWindow();
+        mainWindow.Show();
+    }
 }
