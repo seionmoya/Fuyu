e5e9c0dc60ee0b9b415c089dd4edb7ab98456b7c
静穏靄 <170472707+seionmoya@users.noreply.github.com>
2025-01-10 19:15:38 +0100

    Initial page structure

--- Mods/Launcher/Fuyu.Launcher.Core/src/Mod.cs
+++ Mods/Launcher/Fuyu.Launcher.Core/src/Mod.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using Fuyu.Common.IO;
 using Fuyu.DependencyInjection;
 using Fuyu.Launcher.Common.Services;
 using Fuyu.Modding;
@@ -16,50 +15,49 @@ public class Mod : AbstractMod
     {
         RegisterContent();   
         RegisterMessaging();
-
-        // done loading the mod!
         return Task.CompletedTask;
     }
 
     void RegisterContent()
     {
-        //                 mod folder   http://launcher.fuyu.api/*      res path (use '.' as separator)
-        ContentService.Add(Id,          "favicon.ico",                  "assets.img.favicon.ico");
-        ContentService.Add(Id,          "index.html",                   "index.html");
-        ContentService.Add(Id,          "msg.html",                     "msg.html");
-        ContentService.Add(Id,          "assets/css/bootstrap.min.css", "assets.css.bootstrap.min.css");
-        ContentService.Add(Id,          "assets/css/styles.css",        "assets.css.styles.css");
-        ContentService.Add(Id,          "assets/img/bg-eft.png",        "assets.img.bg-eft.png");
+        //                 mod folder   http://launcher.fuyu.api/*            res path (use '.' as separator)
+        ContentService.Add(Id,          "account-game.html",                  "account-game.html");
+        ContentService.Add(Id,          "account-library.html",               "account-library.html");
+        ContentService.Add(Id,          "account-login.html",                 "account-login.html");
+        ContentService.Add(Id,          "account-register.html",              "account-register.html");
+        ContentService.Add(Id,          "game-library.html",                  "game-library.html");
+        ContentService.Add(Id,          "game-overview.html",                 "game-overview.html");
+        ContentService.Add(Id,          "settings.html",                      "settings.html");
+        ContentService.Add(Id,          "assets/css/bootstrap.min.css",       "assets.css.bootstrap.min.css");
+        ContentService.Add(Id,          "assets/css/styles.css",              "assets.css.styles.css");
+        ContentService.Add(Id,          "assets/js/bootstrap.bundle.min.js",  "assets.js.bootstrap.bundle.min.js");
     }
 
     void RegisterMessaging()
     {
-        //                 page        msg handler
-        MessageService.Add("msg.html", HandleMessage);
+        //                 page                     msg handler
+        MessageService.Add("index.html",            HandleIndexMessage);
+        MessageService.Add("account-game.html",     HandleNull);
+        MessageService.Add("account-library.html",  HandleNull);
+        MessageService.Add("account-login.html",    HandleNull);
+        MessageService.Add("account-register.html", HandleNull);
+        MessageService.Add("game-library.html",     HandleNull);
+        MessageService.Add("game-overview.html",    HandleNull);
+        MessageService.Add("settings.html",         HandleNull);
     }
 
-    void HandleMessage(string message)
+    void HandleNull(string message)
     {
-        // --- EXAMPLE: get current page
-        var url = NavigationService.CurrentPage;
-        Terminal.WriteLine($"The current page url is: {url}");
+        // intentionally empty
+    }
 
-        // --- EXAMPLE: redirect
-        if (message == "NAVIGATE_HOME")
+    void HandleIndexMessage(string message)
+    {
+        if (message == "LOADING_COMPLETED")
         {
-            var newUrl = NavigationService.GetInternalUrl("index.html");
-            NavigationService.Navigate(newUrl);
+            var url = NavigationService.GetInternalUrl("account-login.html");
+            NavigationService.Navigate(url);
             return;
         }
-
-        // --- EXAMPLE: replying
-        var reply = string.Empty;
-
-        if (message == "START_EFT")
-        {
-            reply = "Don't think so!";
-        }
-
-        MessageService.SendMessage(reply);
     }
 }
