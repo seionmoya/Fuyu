b77a10ecfeb8d0f94b8118ccc6545c52d370cfd5
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2025-01-15 12:26:41 -0700

    Added TradersData. It doesn't work?

--- Fuyu.Backend.BSG/Models/ItemEvents/ProfileChange.cs
+++ Fuyu.Backend.BSG/Models/ItemEvents/ProfileChange.cs
@@ -11,6 +11,7 @@ public class ProfileChange
     {
         UnlockedRecipes = [];
         Items = new ItemChanges();
+        TradersData = [];
     }
 
     [DataMember(Name = "experience")]
@@ -21,4 +22,26 @@ public class ProfileChange
 
     [DataMember(Name = "items")]
     public ItemChanges Items { get; set; }
+
+    [DataMember(Name = "traderRelations")]
+    public Dictionary<MongoId, TraderData> TradersData { get; set; }
+}
+
+[DataContract]
+public class TraderData
+{
+    [DataMember(Name = "salesSum")]
+    public long? _salesSum { get; set; }
+
+    [DataMember(Name = "standing")]
+    public double? _standing { get; set; }
+
+    [DataMember(Name = "loyalty")]
+    public float? _loyaltyLevel { get; set; }
+
+    [DataMember(Name = "unlocked")]
+    public bool? _unlocked { get; set; }
+
+    [DataMember(Name = "disabled")]
+    public bool? _disabled { get; set; }
 }
