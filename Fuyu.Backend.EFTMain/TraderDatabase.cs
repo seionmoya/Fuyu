83e6ecee137ebcb0ccfd34c8d77d52e6b5ef9652
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2025-01-15 12:43:02 -0700

    Added trader assorts to csproj and add TraderDatabase assort loading

--- Fuyu.Backend.EFTMain/TraderDatabase.cs
+++ Fuyu.Backend.EFTMain/TraderDatabase.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Fuyu.Backend.BSG.Models.Responses;
 using Fuyu.Backend.BSG.Models.Trading;
 using Fuyu.Backend.BSG.Services;
+using Fuyu.Backend.EFTMain.Services;
 using Fuyu.Common.Collections;
 using Fuyu.Common.Hashing;
 using Fuyu.Common.IO;
@@ -16,6 +18,10 @@ public class TraderDatabase
     private static readonly Lazy<TraderDatabase> instance = new(() => new TraderDatabase());
 
     private readonly ThreadDictionary<MongoId, TraderTemplate> _traders;
+    private readonly ThreadDictionary<MongoId, TraderAssort> _traderAssort;
+
+    private readonly RagfairService _ragfairService;
+    private readonly ItemService _itemService;
 
     /// <summary>
     /// The construction of this class is handled in the <see cref="instance"/> (<see cref="Lazy{T}"/>)
@@ -23,6 +29,9 @@ public class TraderDatabase
     private TraderDatabase()
     {
         _traders = new ThreadDictionary<MongoId, TraderTemplate>();
+        _traderAssort = new ThreadDictionary<MongoId, TraderAssort>();
+        _ragfairService = RagfairService.Instance;
+        _itemService = ItemService.Instance;
     }
 
     public void Load()
@@ -33,11 +42,57 @@ public class TraderDatabase
         foreach (var traderTemplate in body.data)
         {
             _traders.Set(traderTemplate.Id, traderTemplate);
+            try
+            {
+                var assortJson = Resx.GetText("eft",
+                    $"database.client.trading.api.getTraderAssort.{traderTemplate.Id}.json");
+                var traderAssort = Json.Parse<TraderAssort>(assortJson);
+                _traderAssort.Set(traderTemplate.Id, traderAssort);
+
+                var traderRagfairUser = new RagfairTraderUser(traderTemplate.Id);
+
+                foreach (var (itemId, scheme) in traderAssort.BarterScheme)
+                {
+                    var items = _itemService.GetItemAndChildren(traderAssort.Items, itemId);
+                    var handOverRequirements = new List<HandoverRequirement>();
+                    var loyaltyLevel = traderAssort.LoyaltyLevelItems[itemId];
+
+                    foreach (var requirement in scheme)
+                    {
+                        foreach (var requirement2 in requirement)
+                        {
+                            handOverRequirements.Add(new HandoverRequirement
+                            {
+                                Count = (int)requirement2.Count,
+                                TemplateId = requirement2.Template
+                            });
+                        }
+                    }
+
+                    _ragfairService.CreateAndAddOffer(traderRagfairUser, items, false, handOverRequirements, TimeSpan.FromHours(1d), false, loyaltyLevel);
+                }
+
+                Terminal.WriteLine($"Got assort for {traderTemplate.Id}");
+            }
+            catch (FileNotFoundException)
+            {
+                Terminal.WriteLine($"Failed to get assort for {traderTemplate.Id}");
+            }
         }
     }
 
     public Dictionary<MongoId, TraderTemplate> GetTraderTemplates()
     {
         return _traders.ToDictionary();
+    }
+
+    public TraderAssort GetTraderAssort(MongoId traderId)
+    {
+        if (!_traderAssort.TryGet(traderId, out TraderAssort result))
+        {
+            result = null;
+        }
+
+        return result;
     }
 }
