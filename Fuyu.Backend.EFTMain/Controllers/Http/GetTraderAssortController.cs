c1094a8d2a99cdce8ec231eabbc32be4307a959d
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2025-01-15 12:39:44 -0700

    Added loaded trader assort to ragfair

--- Fuyu.Backend.EFTMain/Controllers/Http/GetTraderAssortController.cs
+++ Fuyu.Backend.EFTMain/Controllers/Http/GetTraderAssortController.cs
@@ -1,8 +1,11 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Fuyu.Backend.BSG.Models.Responses;
 using Fuyu.Backend.BSG.Models.Trading;
 using Fuyu.Backend.EFTMain.Networking;
+using Fuyu.Backend.EFTMain.Services;
+using Fuyu.Common.IO;
 using Fuyu.Common.Serialization;
 
 namespace Fuyu.Backend.EFTMain.Controllers.Http;
@@ -12,28 +15,75 @@ public partial class GetTraderAssortController : AbstractEftHttpController
     [GeneratedRegex("/client/trading/api/getTraderAssort/(?<traderId>[A-Za-z0-9]+)")]
     private static partial Regex PathExpression();
 
+    private EftOrm _eftOrm;
+    private TraderOrm _traderOrm;
+
     public GetTraderAssortController() : base(PathExpression())
     {
+        _eftOrm = EftOrm.Instance;
+        _traderOrm = TraderOrm.Instance;
     }
 
     public override Task RunAsync(EftHttpContext context)
     {
+        var profile = _eftOrm.GetActiveProfile(context.SessionId);
         var parameters = context.GetPathParameters(this);
         var traderId = parameters["traderId"];
+        var traderTemplate = _traderOrm.GetTraderTemplate(traderId);
+        var assort = _traderOrm.GetTraderAssort(traderId);
 
-        // TODO: handle this
-        // --seionmoya, 2024-11-18
-        var response = new ResponseBody<TraderAssort>
+        if (assort == null)
         {
-            data = new TraderAssort
+            throw new Exception($"Failed to find assort for trader {traderId}");
+        }
+
+        var assortClone = Json.Clone<TraderAssort>(assort);
+
+        if (!profile.Pmc.TradersInfo.HasValue)
+        {
+            throw new Exception("Player has no TradersInfo");
+        }
+
+        if (!profile.Pmc.TradersInfo.Value.IsValue1)
+        {
+            throw new Exception("TradersInfo is not Dictionary");
+        }
+
+        if (!profile.Pmc.TradersInfo.Value.Value1.TryGetValue(traderId, out var traderInfo))
+        {
+            throw new Exception($"User has no trader info for {traderId}");
+        }
+
+        int level = 0;
+
+        for (int index = 0; index < traderTemplate.LoyaltyLevels.Length; index++)
+        {
+            var loyaltyInfo = traderTemplate.LoyaltyLevels[index];
+            if (loyaltyInfo.MinStanding < traderInfo.standing)
             {
-                ExchangeRate = 0d,
-                Items = [],
-                BarterScheme = [],
-                LoyaltyLevelItems = [],
-                NextResupply = int.MaxValue
+                level = index + 1;
             }
-        };
+        }
+
+        if (level == 0)
+        {
+            throw new Exception("Loyalty level is 0");
+        }
+
+        foreach (var (id, requiredLevel) in assortClone.LoyaltyLevelItems)
+        {
+            if (requiredLevel > level)
+            {
+                assortClone.BarterScheme.Remove(id);
+                var item = assortClone.Items.Find(i => i.Id == id);
+                if (item != null)
+                {
+                    assortClone.Items.Remove(item);
+                }
+            }
+        }
+
+        var response = new ResponseBody<TraderAssort> { data = assortClone };
 
         return context.SendResponseAsync(response, true, true);
     }
