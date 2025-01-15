8eae168d37c49a14acf9e40d5726e83a1089ec99
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2025-01-15 11:33:29 -0700

    Created function instead of inlining it

--- Devtools/Fuyu.Devtool.GenerateFleaMarketOffers/src/Mod.cs
+++ Devtools/Fuyu.Devtool.GenerateFleaMarketOffers/src/Mod.cs
@@ -24,55 +24,76 @@ public class GenerateFleaMarketOffersMod : AbstractMod
     {
         var player = new RagfairPlayerUser(new MongoId(true), 344, EMemberCategory.Developer, EMemberCategory.Developer,
             "b1gdeveloper", 69.420f, true);
-        new Thread((() =>
-        {
-            Terminal.WriteLine("Generating offers...");
-            var sw = Stopwatch.StartNew();
-            var templates = EftOrm.Instance.GetItemTemplates()["data"]!.ToObject<Dictionary<MongoId, ItemTemplate>>();
-            var handbook = EftOrm.Instance.GetHandbook();
-            var success = 0;
-            var failed = 0;
-            
-            foreach (var (tid, template) in templates)
-            {
-                var entry = handbook.Items.Find(i => i.Id == tid);
-                int price = entry?.Price ?? 100;
-                
-                if (price <= 0)
-                {
-                    price = 100;
-                }
-                
-                try
-                {
-                    var item = ItemFactoryService.Instance.CreateItem(template.Id);
-
-                    var x = RagfairService.Instance.CreateAndAddOffer(player,
-                        [item],
-                        false,
-                        [new HandoverRequirement() { Count = price, TemplateId = "5449016a4bdc2d6f028b456f" }],
-                        TimeSpan.FromMinutes(30d),
-                        false
-                    );
-
-                    if (x == null)
-                    {
-                        failed++;
-                    }
-                    else
-                    {
-                        success++;
-                    }
-                }
-                catch (Exception)
-                {
-                    // ignored
-                }
-            }
-
-            Terminal.WriteLine($"Done generating offers: {sw.ElapsedMilliseconds}ms, {success} succeeded and {failed} failed");
-        })).Start();
+        new Thread(GenerateOffers).Start();
 
         return Task.CompletedTask;
+    }
+
+    static void GenerateOffers()
+    {
+        var player = new RagfairPlayerUser(new MongoId(true), 344, EMemberCategory.Developer, EMemberCategory.Developer,
+            "b1gdeveloper", 69.420f, true);
+        Terminal.WriteLine("Generating offers...");
+
+        var sw = Stopwatch.StartNew();
+        var templates = EftOrm.Instance.GetItemTemplates()["data"]!.ToObject<Dictionary<MongoId, ItemTemplate>>();
+        var handbook = EftOrm.Instance.GetHandbook();
+        var success = 0;
+        var failed = 0;
+
+        foreach (var (tid, template) in templates)
+        {
+            if (template.Type != ENodeType.Item)
+            {
+                continue;
+            }
+            
+            var entry = handbook.Items.Find(i => i.Id == tid);
+            int price = entry?.Price ?? 100;
+
+            if (price <= 0)
+            {
+                price = 100;
+            }
+
+            try
+            {
+                var rootItem = ItemFactoryService.Instance.CreateItem(template.Id, out var items);
+                items.Insert(0, rootItem);
+
+                rootItem.Updatable.StackObjectsCount = 100000;
+                
+                var createdOffer = RagfairService.Instance.CreateAndAddOffer(
+                    player,
+                    items,
+                    false,
+                    [
+                        new HandoverRequirement()
+                        {
+                            Count = price,
+                            TemplateId = "5449016a4bdc2d6f028b456f"
+                        }
+                    ],
+                    TimeSpan.FromMinutes(30d),
+                    true
+                );
+
+                if (createdOffer == null)
+                {
+                    failed++;
+                }
+                else
+                {
+                    success++;
+                }
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+        }
+
+        Terminal.WriteLine(
+            $"Done generating offers: {sw.ElapsedMilliseconds}ms, {success} succeeded and {failed} failed");
     }
 }
