851bfd364beb98231269d726302f64228ff0f687
nexus4880 <38168516+nexus4880@users.noreply.github.com>
2025-01-18 16:04:02 -0700

    Added comments to define Union usage

--- Fuyu.Common/Collections/IUnion.cs
+++ Fuyu.Common/Collections/IUnion.cs
@@ -5,12 +5,21 @@ using Newtonsoft.Json.Linq;
 
 namespace Fuyu.Common.Collections;
 
-// NOTE: This type exists so that any Union can be
-// deserialized using the UnionConverter. This is
-// because one may want to add a Union<T1, T2, T3>
-// or even more types later, which this SHOULD
-// support in theory, I have not tested it nor will I
-// -- nexus, 2024-10-22
+// NOTE: Any union member should also use the UnionMappingsAttribute in order to determine which
+// type should be deserialized depending on the token.
+// At the time of writing this it only supports different JTokenTypes.
+/* Example:
+public class UnionUsageExample
+{
+    // This is correct
+    [UnionMappings(JTokenType.Integer, JTokenType.Float)]
+    public Union<int, float> NumericValue { get; set; }
+
+    // This is wrong
+    [UnionMappings(JTokenType.Float, JTokenType.Float)]
+    public Union<float, double> NumericValue2 { get; set; }
+}
+*/
 
 [JsonConverter(typeof(UnionConverter))]
 public interface IUnion
